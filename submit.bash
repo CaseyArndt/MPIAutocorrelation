46a0d8b5a7806f9a2718f01615994e2664d8d18d
CaseyArndt <nada@arch.localdomain>
2021-06-08 10:52:11 -0500

    Change filenames

--- submit.bash
+++ submit.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
-#!SBATCH -J Heat
+#!SBATCH -J AutoC
 #!SBATCH -A cs475-575
 #!SBATCH -p class
 #!SBATCH -N 8   # number of nodes
 #!SBATCH -n 8   # number of tasks
-#!SBATCH -o heat.out
-#!SBATCH -e heat.err
-mpic++ heat.cpp -o heat -lm
-mpiexec -mca btl self,tcp -np 4 heat    # 4 processors
+#!SBATCH -o autoc.out
+#!SBATCH -e autoc.err
+mpic++ autoc.cpp -o autoc -lm
+mpiexec -mca btl self,tcp -np 4 autoc    # 4 processors
